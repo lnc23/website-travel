dfc4dc7601ae0fa9956d8f605129e1859b11cb38
lnc23 <yansenferdinand6@gmail.com>
2022-10-03 20:52:50 +0700

    comit pertama

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import 'assets/scss/style.scss';
+
+function App() {
+  return (
+    <div className="App">
+      hai dunia!
+    </div>
+  );
+}
+
+export default App;
